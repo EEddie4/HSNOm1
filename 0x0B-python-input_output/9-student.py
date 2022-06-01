aad8859c28d0b062033ca3c5b3b59493a821a34b
Nomfutheko <thekothandiwe@gmail.com>
2022-06-01 09:27:08 +0200

    Update and rename 11-student.py to 9-student.py

--- 0x0B-python-input_output/9-student.py
+++ 0x0B-python-input_output/9-student.py
@@ -1,17 +1,18 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+
+'''Task 09 - 9. Student to JSON'''
 
 
-class Student:
-    """Representation of a student"""
+class Student():
+    """class Student"""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        '''Inicilizating ATRIBUTES'''
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self):
-        """returns a dictionary representation of a Student instance"""
+        """Retrieves dictionary of Student"""
         return self.__dict__
