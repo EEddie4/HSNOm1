2949f8529485723e7b3b43c20e7688f8cf2feb2b
Nomfutheko <thekothandiwe@gmail.com>
2022-06-01 09:30:10 +0200

    Update 11-student.py

--- 0x0B-python-input_output/11-student.py
+++ 0x0B-python-input_output/11-student.py
@@ -1,34 +1,34 @@
 #!/usr/bin/python3
-"""
-Contains the clas "Student"
-"""
+
+'''Task 11 - 11. Student to disk and reload'''
 
 
-class Student:
-    """Representation of a student"""
+class Student():
+    """class Student"""
+
     def __init__(self, first_name, last_name, age):
-        """Initializes the student"""
+        '''Inicilizating ATRIBUTES'''
+
         self.first_name = first_name
         self.last_name = last_name
         self.age = age
 
     def to_json(self, attrs=None):
-        """returns a dictionary representation of a Student instance
-        with specified attributes"""
-        if attrs is None:
+        """Retrieves dictionary of Student"""
+
+        if attrs is None or type(attrs) != list:
             return self.__dict__
-        new_dict = {}
-        for a in attrs:
-            try:
-                new_dict[a] = self.__dict__[a]
-            except:
-                pass
-        return new_dict
+        else:
+            tmp = {}
+            for i in attrs:
+                if type(i) != str:
+                    return self.__dict__
+                if i in self.__dict__.keys():
+                    tmp[i] = self.__dict__[i]
+            return tmp
 
     def reload_from_json(self, json):
-        """replaces all attributes of the Student instance"""
-        for key in json:
-            try:
-                setattr(self, key, json[key])
-            except:
-                pass
+        """Replaces all items in Json"""
+
+        for a in json.keys():
+            self.__dict__[a] = json[a]
