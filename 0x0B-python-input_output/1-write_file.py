639bdf59d1ef8e5b0a82501f34fbdeb931bc1bd1
Nomfutheko <thekothandiwe@gmail.com>
2022-06-01 09:13:48 +0200

    Update 1-write_file.py

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -1,7 +1,10 @@
 #!/usr/bin/python3
+
+'''Task 01 - 1. Write to a file'''
+
+
 def write_file(filename="", text=""):
-    """writes a string to a text file (UTF8) and returns the number of"""
-    """characters written"""
-    with open(filename, mode="w+", encoding="utf-8") as f:
-        chars = f.write(text)
-    return chars
+    '''function that writes a string to a text file (UTF8)
+    and returns the number of characters written'''
+    with open(filename, mode='w', encoding='utf-8') as f:
+        return f.write(text)
